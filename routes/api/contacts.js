e41f1c68efb00a468065a71637d47c29814cc007
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-03 23:07:40 +0300

    fix lint

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,80 +4,80 @@ const Contacts = require('../../model')
 const { validateContact, validateContactId } = require('./validation')
 
 router.get('/', async (req, res, next) => {
-	try {
-		const contacts = await Contacts.listContacts()
-		res.json({ status: 'success', code: 200, data: { contacts } })
-	} catch (error) {
-		next(error)
-	}
+  try {
+    const contacts = await Contacts.listContacts()
+    res.json({ status: 'success', code: 200, data: { contacts } })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', validateContactId, async (req, res, next) => {
-	try {
-		const contact = await Contacts.getContactById(req.params.contactId)
-		if (contact) {
-			return res
-				.status(201)
-				.json({ status: 'success', code: 201, data: { contact } })
-		}
-		return res
-			.status(404)
-			.json({ status: 'error', code: 404, message: 'Not Found' })
-	} catch (error) {
-		next(error)
-	}
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId)
+    if (contact) {
+      return res
+        .status(201)
+        .json({ status: 'success', code: 201, data: { contact } })
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not Found' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', validateContact, async (req, res, next) => {
-	try {
-		const contact = await Contacts.addContact(req.body)
+  try {
+    const contact = await Contacts.addContact(req.body)
 
-		return res
-			.status(201)
-			.json({ status: 'success', code: 201, data: { contact } })
-	} catch (error) {
-		next(error)
-	}
+    return res
+      .status(201)
+      .json({ status: 'success', code: 201, data: { contact } })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', validateContactId, async (req, res, next) => {
-	try {
-		const contact = await Contacts.removeContact(req.params.contactId)
-		if (contact) {
-			return res
-				.status(201)
-				.json({ status: 'success', code: 201, data: { contact } })
-		}
-		return res
-			.status(404)
-			.json({ status: 'error', code: 404, message: 'Not Found' })
-	} catch (error) {
-		next(error)
-	}
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId)
+    if (contact) {
+      return res
+        .status(201)
+        .json({ status: 'success', code: 201, data: { contact } })
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not Found' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.put(
-	'/:contactId',
-	validateContactId,
-	validateContact,
-	async (req, res, next) => {
-		try {
-			const contact = await Contacts.updateContact(
-				req.params.contactId,
-				req.body
-			)
-			if (contact) {
-				return res
-					.status(201)
-					.json({ status: 'success', code: 201, data: { contact } })
-			}
-			return res
-				.status(404)
-				.json({ status: 'error', code: 404, message: 'Not Found' })
-		} catch (error) {
-			next(error)
-		}
-	}
+  '/:contactId',
+  validateContactId,
+  validateContact,
+  async (req, res, next) => {
+    try {
+      const contact = await Contacts.updateContact(
+        req.params.contactId,
+        req.body
+      )
+      if (contact) {
+        return res
+          .status(201)
+          .json({ status: 'success', code: 201, data: { contact } })
+      }
+      return res
+        .status(404)
+        .json({ status: 'error', code: 404, message: 'Not Found' })
+    } catch (error) {
+      next(error)
+    }
+  }
 )
 
 module.exports = router
