790ca6784b3b7e3cdfb34947b36bc398abd74382
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-11 23:02:03 +0300

    mangodb mangoose

---
+++ config/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const uri = process.env.URL_DB
+const db = mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connection to DB')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connection error ${err.message}`)
+})
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close()
+  console.log('Connection to DB closed')
+  process.exit()
+})
+
+// client.connect((err) => {
+// const collection = client.db('test').collection('devices')
+// perform actions on the collection object
+// client.close()
+// })
+
+module.exports = db
