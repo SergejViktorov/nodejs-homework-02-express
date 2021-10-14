8b678e03722504cd510f03637f4f45acd4c3d03d
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-14 09:13:46 +0300

    lint-fix

--- config/db.js
+++ config/db.js
@@ -4,22 +4,22 @@ require('dotenv').config()
 const uri = process.env.URL_DB
 console.log('uri', uri)
 const db = mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 
 mongoose.connection.on('connected', () => {
-	console.log('Mongoose connection to DB')
+  console.log('Mongoose connection to DB')
 })
 
 mongoose.connection.on('error', (err) => {
-	console.log(`Mongoose connection error ${err.message}`)
+  console.log(`Mongoose connection error ${err.message}`)
 })
 
 process.on('SIGINT', async () => {
-	await mongoose.connection.close()
-	console.log('Connection to DB closed')
-	process.exit()
+  await mongoose.connection.close()
+  console.log('Connection to DB closed')
+  process.exit()
 })
 
 module.exports = db
