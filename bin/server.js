8b3fc830e8857ef764b6227cea678b5fe1cd2e46
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-14 07:17:54 +0300

    gitignore env

--- bin/server.js
+++ bin/server.js
@@ -4,9 +4,9 @@ const app = require('../app')
 const PORT = process.env.PORT || 3000
 
 db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`)
-  })
+	app.listen(PORT, () => {
+		console.log(`Server running. Use our API on port: ${PORT}`)
+	})
 }).catch((err) => {
-  console.log(`Server not run. Error: ${err.message}`)
+	console.log(`Server not run. Error: ${err.message}`)
 })
