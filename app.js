21e1e85d24d4f49ee9ba7eb004817b5803819993
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-16 22:45:07 +0300

    /

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/contacts/contacts')
+const usersRouter = require('./routes/users/users')
 
 const app = express()
 
@@ -12,14 +13,20 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  if (err.name === 'ValidationError') {
+    return res
+      .status(400)
+      .json({ status: 'fail', code: 400, message: err.message })
+  }
+  res.status(500).json({ status: 'fail', code: 500, message: err.message })
 })
 
 module.exports = app
---
+++ routes/contacts/validation.js
@@ -0,0 +1,48 @@
+const Joi = require('joi')
+// Joi.objectId = require('joi-objectId')(Joi)
+
+const { ValidInContact } = require('../../config/constants')
+
+const schemaContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .min(ValidInContact.MIN)
+    .max(ValidInContact.MAX)
+    .required(),
+  isFavorite: Joi.boolean().optional(),
+})
+
+const schemaStatusContact = Joi.object({
+  isFavorite: Joi.boolean().required(),
+})
+
+const schemaContactId = Joi.object({
+  contactId: Joi.string().required(),
+})
+
+const validate = async (schema, obj, res, next) => {
+  try {
+    await schema.validateAsync(obj)
+    next()
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: 'error',
+      code: 400,
+      message: `missing required ${err.message.replace(/"/g, '')} field`,
+    })
+  }
+}
+
+module.exports.validateContact = async (req, res, next) => {
+  return await validate(schemaContact, req.body, res, next)
+}
+
+module.exports.validateStatusContact = async (req, res, next) => {
+  return await validate(schemaStatusContact, req.body, res, next)
+}
+
+module.exports.validateContactId = async (req, res, next) => {
+  return await validate(schemaContactId, req.params, res, next)
+}
