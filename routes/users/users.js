c914e2600d0314596b8d1094d59f20bd73d8eaee
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-10-21 15:14:17 +0300

    current

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { registration, login, logout } = require('../../controllers/users')
+const {
+  registration,
+  login,
+  logout,
+  current,
+} = require('../../controllers/users')
 const guard = require('../../helpers/guard')
 const loginLimit = require('../../helpers/rate-limit-login')
 
 router.post('/registration', registration)
 router.post('/login', loginLimit, login)
 router.post('/logout', guard, logout)
+router.get('/current', guard, current)
 
 module.exports = router
